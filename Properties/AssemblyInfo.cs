fd8175a42cb15f5a352e6797928478dca7b4b4d9
Bernt Johnsen <39300332+berntjoh@users.noreply.github.com>
2022-03-01 12:51:49 +0100

    Update AssemblyInfo.cs
    Uppdated assemblyVersion and AssemblyFileversion lines to match with publish steps

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-/*[assembly: AssemblyInformationalVersion("1.1.1")]*/
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -31,5 +34,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9ad32772-985c-408a-8293-6f8e93c70ce5")]
-[assembly: AssemblyInformationalVersion("1.0.0")] //File version
-[assembly: AssemblyFileVersion("1.0.0")] //Product version
